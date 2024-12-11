4671fa4d69795734040ef923eb3062720ab8fc73
HeroMaster137 <73035479+HeroMaster137@users.noreply.github.com>
2024-12-11 10:13:06 -0800

    Make it look slightly better-ish

--- main.js
+++ main.js
@@ -85,7 +85,29 @@ function load(){
     }
     document.getElementById('teamAdd').innerHTML = Math.round((teamWorth+((heliWorth+(games*gameWorth))*helis))*10*tuesdayWorth)/10;
     document.getElementById('heliAdd').innerHTML = Math.round((0.5+(gameWorth*games))*10)/10;
+    openTab(event, 'Upgrades');
 }
+
+function openTab(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
 
 function deleteSave(){
     localStorage.removeItem("save");
